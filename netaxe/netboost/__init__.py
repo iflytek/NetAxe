3b65894b631dfb41ea319148f22d847d85f9c764
zjwu10 <zjwu10@iflytek.com>
2022-10-11 17:39:02 +0800

    优化 nacos 修复 merge问题

--- netaxe/netboost/__init__.py
+++ netaxe/netboost/__init__.py
@@ -1,7 +1,11 @@
 from __future__ import absolute_import, unicode_literals
+import logging
 import pymysql
+import threading
 from .celery import app as celery_app
-import KcangNacos.nacos as nacos
+from utils.custom.nacos import nacos
+
+logger = logging.getLogger('server')
 
 # Fake PyMySQL's version and install as MySQLdb
 # https://adamj.eu/tech/2020/02/04/how-to-use-pymysql-with-django/
@@ -9,7 +13,7 @@ pymysql.version_info = (1, 4, 2, "final", 0)
 pymysql.install_as_MySQLdb()
 
 # 注册服务
-nacosServer = nacos.nacos(ip="10.254.2.188", port=8848)
+nacosServer = nacos(ip="10.254.2.188", port=8848)
 nacosServer.registerService(serviceIp="10.254.2.188", servicePort="8001", serviceName="auth", groupName="default")
 nacosServer.healthyCheck()
 
