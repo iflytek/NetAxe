8db8ef5017c5ebd348e4a6d3e3afcb73b55e9d0c
M87NET <M87NET@github.com>
2023-03-22 16:16:15 +0800

    修复设备删除的问题2

--- netaxe/apps/asset/models.py
+++ netaxe/apps/asset/models.py
@@ -421,7 +421,6 @@ class NetworkDevice(models.Model):
     account = models.ManyToManyField('AssetAccount', verbose_name='管理账户', blank=True)
     plan = models.ForeignKey("automation.CollectionPlan", verbose_name='采集方案',
                              blank=True, null=True, related_name='releate_device', on_delete=models.SET_NULL)
-    # bgbu = models.ManyToManyField("users.BgBu", verbose_name='BGBU', blank=True)
     history = HistoricalRecords()
 
     def __str__(self):
