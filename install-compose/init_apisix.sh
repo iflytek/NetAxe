6cbb5357bb73767d966ba41c6311414811fc62f2
M87NET <M87NET@github.com>
2023-06-09 10:56:53 +0800

    更新readme

--- install-compose/init_apisix.sh
+++ install-compose/init_apisix.sh
@@ -116,7 +116,7 @@ curl http://127.0.0.1:9080/apisix/admin/routes \
   },
   "status": 1
 }'
-# 初始化cmdb-ipam
+# 初始化基础平台
 curl http://127.0.0.1:9080/apisix/admin/routes \
 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X POST -i -d '
 {
@@ -151,6 +151,50 @@ curl http://127.0.0.1:9080/apisix/admin/routes \
     },
     "pass_host": "pass",
     "service_name": "ipam",
+    "keepalive_pool": {
+      "idle_timeout": 60,
+      "requests": 1000,
+      "size": 320
+    }
+  },
+  "status": 1
+}'
+
+# 初始化告警平台
+curl http://127.0.0.1:9080/apisix/admin/routes \
+-H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X POST -i -d '
+{
+  "uri": "/alert_gateway/*",
+  "name": "alert_gateway",
+  "plugins": {
+    "forward-auth": {
+      "client_headers": [],
+      "disable": false,
+      "request_headers": [
+        "Authorization"
+      ],
+      "ssl_verify": false,
+      "timeout": 10000,
+      "upstream_headers": [
+        "Authorization"
+      ],
+      "uri": "http://rbac-nginx:80/rbac/status/"
+    }
+  },
+  "upstream": {
+    "timeout": {
+      "connect": 6,
+      "send": 6,
+      "read": 6
+    },
+    "type": "roundrobin",
+    "scheme": "http",
+    "discovery_type": "nacos",
+    "discovery_args": {
+      "group_name": "default"
+    },
+    "pass_host": "pass",
+    "service_name": "alert_gateway",
     "keepalive_pool": {
       "idle_timeout": 60,
       "requests": 1000,
