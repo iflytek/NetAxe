03161334ad2daae744ca80e6b56fa871f187837f
M87NET <M87NET@github.com>
2023-06-09 16:22:13 +0800

    更新readme

--- install-compose/mysql-compose/init/netaxe.sql
+++ install-compose/mysql-compose/init/netaxe.sql
@@ -1,3 +1,27 @@
 CREATE DATABASE IF NOT EXISTS netaxe
 DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_general_ci;
+
+CREATE DATABASE IF NOT EXISTS rbac
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
+
+CREATE DATABASE IF NOT EXISTS msggateway
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
+
+CREATE DATABASE IF NOT EXISTS ipam
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
+
+CREATE DATABASE IF NOT EXISTS alertgateway
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
+
+
+create user netaxe@localhost identified by 'netaxe_pwd';
+grant all on *.* to netaxe@'%' identified by 'netaxe_pwd';
+grant all on *.* to netaxe@localhost identified by 'netaxe_pwd';
+grant all on *.* to netaxe@'%' identified by 'netaxe_pwd' with grant option;
+grant all privileges on *.* to root@'%' identified by 'root_devnet@2022';
+flush privileges;
