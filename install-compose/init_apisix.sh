ddfb52dd41a87f313c8f53ac9d881f41fb3b4191
M87NET <M87NET@github.com>
2023-08-11 14:42:15 +0800

    feature: 监控中心上线5

--- install-compose/init_apisix.sh
+++ install-compose/init_apisix.sh
@@ -247,4 +247,47 @@ curl http://127.0.0.1:9080/apisix/admin/routes \
   },
   "status": 1
 }'
+# 初始化监控中心
+curl http://127.0.0.1:9080/apisix/admin/routes \
+-H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X POST -i -d '
+{
+  "uri": "/neteye/*",
+  "name": "neteye",
+  "plugins": {
+    "forward-auth": {
+      "client_headers": [],
+      "disable": false,
+      "request_headers": [
+        "Authorization"
+      ],
+      "ssl_verify": false,
+      "timeout": 10000,
+      "upstream_headers": [
+        "Authorization"
+      ],
+      "uri": "http://rbac-nginx:80/rbac/status/"
+    }
+  },
+  "upstream": {
+    "timeout": {
+      "connect": 6,
+      "send": 6,
+      "read": 6
+    },
+    "type": "roundrobin",
+    "scheme": "http",
+    "discovery_type": "nacos",
+    "discovery_args": {
+      "group_name": "default"
+    },
+    "pass_host": "pass",
+    "service_name": "neteye",
+    "keepalive_pool": {
+      "idle_timeout": 60,
+      "requests": 1000,
+      "size": 320
+    }
+  },
+  "status": 1
+}'
 echo "------------------初始化apisix完成----------------------"
