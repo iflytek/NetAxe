0c2437d789d44bb26c667280f3dcdc4d1f09b4d5
zjwu10 <zjwu10@iflytek.com>
2024-07-31 16:57:51 +0800

    pref: 初始化IP

--- install-compose/deploy.sh
+++ install-compose/deploy.sh
@@ -16,6 +16,20 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+if [ $# -eq 0 ]; then
+  # 如果没有传入参数，则使用默认网卡的IP地址
+  default_iface=$(ip route show default | awk '/default/ {print $5}')
+  iface_ip=$(ip addr show dev $default_iface | awk '/inet / {print $2}' | cut -d '/' -f 1)
+else
+  # 如果传入了参数，则使用用户输入的IP地址
+  iface_ip=$1
+fi
+
+# 遍历当前目录的所有子目录，查找 config.json 文件并修改其中的 server_ip 字段
+#find . -type f -name "config.json" -exec sed -i "s/\"server_ip\": \"[^\"]*\"/\"server_ip\": \"$iface_ip\"/g" {} \;
+find . -type f -name "config.json" -exec sed -i "s/tmp_server_ip/$iface_ip/g" {} \;
+echo "Using IP address: $iface_ip"
+echo "------------------初始化IP完成----------------------"
 
 # 创建docker_netaxe网络
 docker network inspect docker_netaxe
--- install-compose/init.sh
+++ install-compose/init.sh
@@ -1,20 +1,5 @@
 #! /bin/bash
 
-if [ $# -eq 0 ]; then
-  # 如果没有传入参数，则使用默认网卡的IP地址
-  default_iface=$(ip route show default | awk '/default/ {print $5}')
-  iface_ip=$(ip addr show dev $default_iface | awk '/inet / {print $2}' | cut -d '/' -f 1)
-else
-  # 如果传入了参数，则使用用户输入的IP地址
-  iface_ip=$1
-fi
-
-# 遍历当前目录的所有子目录，查找 config.json 文件并修改其中的 server_ip 字段
-#find . -type f -name "config.json" -exec sed -i "s/\"server_ip\": \"[^\"]*\"/\"server_ip\": \"$iface_ip\"/g" {} \;
-find . -type f -name "config.json" -exec sed -i "s/tmp_server_ip/$iface_ip/g" {} \;
-echo "Using IP address: $iface_ip"
-echo "------------------初始化IP完成----------------------"
-
 curl -X PUT 'http://127.0.0.1:8848/nacos/v1/auth/users?username=nacos&newPassword=netaxenacos'
 echo "------------------初始化nacos密码完成----------------------"
 
