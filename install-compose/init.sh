51606b72fcfbe691a3aa5d9b16358d4b52d5248f
zjwu10 <zjwu10@iflytek.com>
2024-08-30 10:26:11 +0800

    pref：初始化脚本、开启nacos认证模式

--- install-compose/init.sh
+++ install-compose/init.sh
@@ -1,14 +1,9 @@
 #! /bin/bash
-
-curl -X PUT 'http://127.0.0.1:8848/nacos/v1/auth/users?username=nacos&newPassword=netaxenacos'
-echo "------------------初始化nacos密码完成----------------------"
-
 default_key=$(openssl rand -hex 16)
 
 echo "开始进行初始化操作，本操作将一次性生成各个配置文件的秘钥"
 echo "Using IP address: $iface_ip"
 echo "Using key: $default_key"
-
 
 
 # 遍历当前目录的所有子目录，查找 config.json 文件并修改其中的 server_ip 字段
@@ -25,15 +20,17 @@ find ./mysql-compose -type f -name "docker-compose.yml" -exec sed -i "s/MYSQL_PA
 find ./redis-compose -type f -name "docker-compose.yml" -exec sed -i "s/REDIS_PASSWORD/$default_key/g" {} \;
 find ./mongo-compose -type f -name "docker-compose.yml" -exec sed -i "s/MONGO_PASSWORD/$default_key/g" {} \;
 find ./rabbitmq-compose -type f -name "docker-compose.yml" -exec sed -i "s/RABBITMQ_PASSWORD/$default_key/g" {} \;
-find . -type f -name "init_apisix.sh" -exec sed -i "s/DJANGO_INSECURE/$default_key/g" {} \;
-find . -type f -name "init_apisix.sh" -exec sed -i "s/APISIX_ADMIN_KEY/$default_key/g" {} \;
-find . -type f -name "init_apisix.sh" -exec sed -i "s/NACOS_PASSWORD/$default_key/g" {} \;
+find ./nacos-compose -type f -name "docker-compose.yaml" -exec sed -i "s/NACOS_KEY/$default_key/g" {} \;
+# find . -type f -name "init_apisix.sh" -exec sed -i "s/DJANGO_INSECURE/$default_key/g" {} \;
+# find . -type f -name "init_apisix.sh" -exec sed -i "s/APISIX_ADMIN_KEY/$default_key/g" {} \;
+# find . -type f -name "init_apisix.sh" -exec sed -i "s/NACOS_PASSWORD/$default_key/g" {} \;
 
-
+curl -X POST 'http://127.0.0.1:8848/nacos/v1/auth/users/admin' -d 'password=${default_key}'
+echo "------------------初始化nacos密码完成----------------------"
 
 # 初始化消费者
 curl http://127.0.0.1:9080/apisix/admin/consumers \
--H 'X-API-KEY: APISIX_ADMIN_KEY' -X PUT -i -d '
+-H 'X-API-KEY: ${default_key}' -X PUT -i -d '
 {
   "username": "jwt_auth",
   "plugins": {
@@ -43,14 +40,14 @@ curl http://127.0.0.1:9080/apisix/admin/consumers \
       },
       "exp": 86400,
       "key": "apisix",
-      "secret": "DJANGO_INSECURE"
+      "secret": ${default_key}
     }
   }
 }'
 
 # 初始化认证
 curl http://127.0.0.1:9080/apisix/admin/routes \
--H 'X-API-KEY: APISIX_ADMIN_KEY' -X POST -i -d '
+-H 'X-API-KEY: ${default_key}' -X POST -i -d '
 {
   "uri": "/rbac/login/",
   "name": "登录",
@@ -79,7 +76,7 @@ curl http://127.0.0.1:9080/apisix/admin/routes \
 
 # 初始化权限中心
 curl http://127.0.0.1:9080/apisix/admin/routes \
--H 'X-API-KEY: APISIX_ADMIN_KEY' -X POST -i -d '
+-H 'X-API-KEY: ${default_key}' -X POST -i -d '
 {
   "uri": "/rbac/*",
   "name": "权限中心",
@@ -115,7 +112,7 @@ curl http://127.0.0.1:9080/apisix/admin/routes \
 
 # 初始化cmdb-api
 curl http://127.0.0.1:9080/apisix/admin/routes \
--H 'X-API-KEY: APISIX_ADMIN_KEY' -X POST -i -d '
+-H 'X-API-KEY: ${default_key}' -X POST -i -d '
 {
   "uri": "/api/*",
   "name": "cmdb-api",
@@ -150,7 +147,7 @@ curl http://127.0.0.1:9080/apisix/admin/routes \
 }'
 # 初始化cmdb-ipam
 curl http://127.0.0.1:9080/apisix/admin/routes \
--H 'X-API-KEY: APISIX_ADMIN_KEY' -X POST -i -d '
+-H 'X-API-KEY: ${default_key}' -X POST -i -d '
 {
   "uri": "/ipam/*",
   "name": "cmdb-ipam",
@@ -186,7 +183,7 @@ curl http://127.0.0.1:9080/apisix/admin/routes \
 
 # 初始化cmdb-base_platform
 curl http://127.0.0.1:9080/apisix/admin/routes \
--H 'X-API-KEY: APISIX_ADMIN_KEY' -X POST -i -d '
+-H 'X-API-KEY: ${default_key}' -X POST -i -d '
 {
   "uri": "/base_platform/*",
   "name": "cmdb-base_platform",
@@ -221,7 +218,7 @@ curl http://127.0.0.1:9080/apisix/admin/routes \
 }'
 # 初始化alert-gateway
 curl http://127.0.0.1:9080/apisix/admin/routes \
--H 'X-API-KEY: APISIX_ADMIN_KEY' -X POST -i -d '
+-H 'X-API-KEY: ${default_key}' -X POST -i -d '
 {
   "uri": "/alert_gateway/*",
   "name": "alert_gateway",
@@ -256,7 +253,7 @@ curl http://127.0.0.1:9080/apisix/admin/routes \
 }'
 # 初始化message-gateway
 curl http://127.0.0.1:9080/apisix/admin/routes \
--H 'X-API-KEY: APISIX_ADMIN_KEY' -X POST -i -d '
+-H 'X-API-KEY: ${default_key}' -X POST -i -d '
 {
   "uri": "/msg_gateway/*",
   "name": "msg_gateway",
@@ -290,7 +287,3 @@ curl http://127.0.0.1:9080/apisix/admin/routes \
   "status": 1
 }'
 echo "------------------初始化apisix完成----------------------"
-
-curl -X PUT 'http://127.0.0.1:8848/nacos/v1/auth/users?username=nacos&newPassword=NACOS_PASSWORD'
-echo "------------------初始化nacos密码完成----------------------"
-
