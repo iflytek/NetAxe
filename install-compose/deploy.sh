bd88e9aa0b085ca2b72fe2e52f5df90fb987d82a
zjwu10 <zjwu10@iflytek.com>
2024-09-02 13:49:41 +0800

    fix: nacos修改默认密码部署脚本问题

--- install-compose/deploy.sh
+++ install-compose/deploy.sh
@@ -55,8 +55,8 @@ find ./rabbitmq-compose -type f -name "docker-compose.yml" -exec sed -i "s/RABBI
 
 
 sed -i "s/NACOS_KEY/${nacos_key}/g" ./nacos-compose/docker-compose.yml
-sed -i "s/APISIX_ADMIN_KEY/$default_key/g" {} ./init.sh
-sed -i "s/NACOS_PASSWORD/$default_key/g" {} ./init.sh
+sed -i "s/APISIX_ADMIN_KEY/$default_key/g" ./init.sh
+sed -i "s/NACOS_PASSWORD/$default_key/g" ./init.sh
 
 # find ./nacos-compose -type f -name "docker-compose.yml" -exec sed -i "s/NACOS_KEY/${nacos_key}/g" {} \;
 # find . -type f -name "init.sh" -exec sed -i "s/APISIX_ADMIN_KEY/$default_key/g" {} \;
