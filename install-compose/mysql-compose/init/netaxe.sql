ef04c7097cf401765495168f2bcab2a76bd3c76a
zjwu10 <zjwu10@iflytek.com>
2024-09-03 15:50:11 +0800

    fix：MySQL密码初始化问题

--- install-compose/mysql-compose/init/netaxe.sql
+++ install-compose/mysql-compose/init/netaxe.sql
@@ -18,5 +18,5 @@ create user netaxe@localhost identified by 'netaxe_pwd';
 grant all on *.* to netaxe@'%' identified by 'netaxe_pwd';
 grant all on *.* to netaxe@localhost identified by 'netaxe_pwd';
 grant all on *.* to netaxe@'%' identified by 'netaxe_pwd' with grant option;
-grant all privileges on *.* to root@'%' identified by 'root_devnet@2022';
+grant all privileges on *.* to root@'%' identified by 'MYSQL_PASSWORD';
 flush privileges;
