eff12b30e41aade234ca0eb3e6af1c2b097620ab
jmli12 <jmli12@iflytek.com>
2025-08-12 16:20:05 +0800

    管控平台增加配置备份目录的初始化

--- install-compose/deploy.sh
+++ install-compose/deploy.sh
@@ -159,6 +159,8 @@ cd $current_path
 cd baseplatform-compose
 git clone -b dev https://gitee.com/NetAxeClub/base-platform.git
 mv config.json base-platform/config/
+mkdir base-platform/backend/media/device_config/current-configuration
+mkdir base-platform/backend/media/device_config/startup-configuration
 docker-compose pull
 docker-compose  up -d
 echo "------------------管控平台状态------------------"
